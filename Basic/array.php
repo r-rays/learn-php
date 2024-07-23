0e96d83a1caf36e6e262e2f50e4055b37c22d4e8
Rieky Rayson <r.rayyysss@gmail.com>
2024-07-23 18:38:37 +0700

    Associative Array

--- Basic/array.php
+++ Basic/array.php
@@ -94,3 +94,54 @@ $mahasiswa = [["Rieky Rayson", "3505171511040003", "Rekayasa Perangkat Lunak", "
 </body>
 
 </html>
+
+<?php
+echo "<br>";
+echo "<br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+    <style>
+        .kotak {
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+            float: left;
+            margin: 3px;
+            transition: 1s;
+        }
+
+        .kotak:hover {
+            transform: rotate(360deg);
+            border-radius: 50%;
+        }
+
+        .clear {
+            clear: both;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $angka = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    // echo $angka[2][1]
+    ?>
+
+    <?php foreach ($angka as $a) : ?>
+        <?php foreach ($a as $b) : ?>
+            <div class="kotak"><?= $b; ?></div>
+        <?php endforeach; ?>
+        <div class="clear"></div>
+    <?php endforeach; ?>
+</body>
+
+</html>
---
+++ Basic/associative-array.php
@@ -0,0 +1,39 @@
+<?php
+$mahasiswa = [
+    [
+        "nama" => "Rieky Rayson",
+        "nrp" => "3505171511040003",
+        "jurusan" => "Rekayasa Perangkat Lunak",
+        "email" => "r.rayyysss@gmail.com"
+    ],
+    [
+        "nama" => "Fitaria Gustina",
+        "nrp" => "3505171511040003",
+        "jurusan" => "Manajemen",
+        "email" => "greenfithea@gmail.com"
+    ]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Associative</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <?php foreach ($mahasiswa as $mhs) : ?>
+        <ul>
+            <li>Nama : <?= $mhs["nama"]; ?></li>
+            <li>NRP : <?= $mhs["nrp"]; ?></li>
+            <li>Jurusan : <?= $mhs["jurusan"]; ?></li>
+            <li>Email : <?= $mhs["email"]; ?></li>
+        </ul>
+    <?php endforeach; ?>
+</body>
+
+</html>
